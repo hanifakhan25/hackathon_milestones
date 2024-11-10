b04345d9ae0a9898fc7cb1fd07c42feda5c3905f
hanifakhan25 <160266583+hanifakhan25@users.noreply.github.com>
2024-11-10 23:44:42 +0500

    Update script.js

--- milestone3/script.js
+++ milestone3/script.js
@@ -13,8 +13,8 @@ form.addEventListener('submit', function (event) {
     //generate the resume constant dynamic
     var resumeHTML = "\n   <h2><b>Resume</h2></b>\n   <h3> Persnoal Information </h3>\n   <p><b>Name:</b>".concat(name, "</p>\n   <p><b>Email:</b>").concat(email, "</p>\n   <p><b>Phone:</b>").concat(phone, "</p>\n\n   <h3>Education</h3>\n   <p>").concat(education, "</p>\n\n   <h3>Experience</h3>\n   <p>").concat(experience, "</p>\n\n   <h3>Skills</h3>\n   <p>").concat(skills, "</p>\n    ");
     //display the generated resume
-    if (resumedisplayElement) {
-        resumedisplayElement.innerHTML = resumeHTML;
+    if (resumeDisplayElement) {
+        resumeDisplayElement.innerHTML = resumeHTML;
     }
     else {
         console.error('the resume display element is missing');
